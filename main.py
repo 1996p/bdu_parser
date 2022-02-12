61a82db1e62f884a772633ed2105a05a702bc813
1996p <dursinear@gmail.com>
2022-02-12 19:42:37 +0700

    same logic but multithreading

--- main.py
+++ main.py
@@ -2,61 +2,78 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import os
+import threading
 
 
-class Parser:
+class Parser(threading.Thread):
     HEADERS = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36',
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     }
 
-    def __init__(self):
-        self.url = 'https://bdu.fstec.ru/threat'
-        self.template_url = 'https://bdu.fstec.ru/threat/ubi.'
-        self.qty_of_elements = 0
-        self.threats = []
+    URL = 'https://bdu.fstec.ru/threat'
+    TEMPLATE_URL = 'https://bdu.fstec.ru/threat/ubi.'
 
-    def get_qty_elements(self):
-        q = requests.get(url=self.url, headers=self.HEADERS)
-        soup = BeautifulSoup(q.text, 'lxml')
-        self.qty_of_elements = int(soup.find('div', class_='summary').text.split()[-1])
+    def __init__(self, threats_list, threat_number, lock, *args, **kwargs):
+        super(Parser, self).__init__(*args, **kwargs)
+        self.qty_of_elements = 0
+        self.threats = threats_list
+        self.threat_number = threat_number
+        self.lock = lock
 
     def run(self):
-        self.get_qty_elements()
+        if self.threat_number < 10:
+            threat_number = f'00{self.threat_number}'
+        elif self.threat_number < 100:
+            threat_number = f'0{self.threat_number}'
+        else:
+            threat_number = str(self.threat_number)
 
-        for i in range(1, self.qty_of_elements + 1):
-            if i < 10:
-                threat_number = f'00{i}'
-            elif i < 100:
-                threat_number = f'0{i}'
-            else:
-                threat_number = str(i)
+        threat_url = self.TEMPLATE_URL + threat_number
+        print(f'Go to {threat_url}..')
 
-            threat_url = self.template_url + threat_number
-            print(f'Go to {threat_url}..')
+        response = requests.get(url=threat_url, headers=self.HEADERS)
+        soup = BeautifulSoup(response.text, 'lxml')
+        container = soup.find('div', class_='container-fluid').find('div', class_='row')
+        threat_name = container.find('h4').text
+        table = container.find('table', class_='attr-view-table').find_all('tr')
 
-            response = requests.get(url=threat_url, headers=self.HEADERS)
-            soup = BeautifulSoup(response.text, 'lxml')
-            container = soup.find('div', class_='container-fluid').find('div', class_='row')
-            threat_name = container.find('h4').text
-            table = container.find('table', class_='attr-view-table').find_all('tr')
+        data_dict = {threat_name[:7]: threat_name[9:]}
 
-            data_dict = {threat_name[:7]: threat_name[9:]}
-
-            for parameter in table:
-                parameter_info = parameter.find_all('td')
-                data_dict[parameter_info[0].text.strip()] = parameter_info[1].text.strip().replace('\n', ',')
-
+        for parameter in table:
+            parameter_info = parameter.find_all('td')
+            data_dict[parameter_info[0].text.strip()] = parameter_info[1].text.strip().replace('\n', ',')
+        with self.lock:
             self.threats.append(data_dict)
 
-        if not os.path.exists(os.curdir + '/data'):
-            os.mkdir(os.curdir + '/data')
-            print('Folder for data was created!')
-
-        with open('data/threats.json', 'w', encoding='utf-8') as file:
-            json.dump(self.threats, file, indent=4, ensure_ascii=False)
+    @classmethod
+    def get_qty_elements(cls):
+        q = requests.get(url=cls.URL, headers=cls.HEADERS)
+        soup = BeautifulSoup(q.text, 'lxml')
+        qty_of_elements = int(soup.find('div', class_='summary').text.split()[-1])
+        return qty_of_elements
 
 
 if __name__ == '__main__':
-    pars = Parser()
-    pars.run()
+    threats = []
+    threat_count = Parser.get_qty_elements()
+
+    lock = threading.Lock()
+    parsers = [Parser(threats_list=threats, threat_number=i, lock=lock) for i in range(1, threat_count + 1)]
+
+    for parser in parsers:
+        parser.start()
+
+    print('Threads were started, now just waitin...')
+
+    for parser in parsers:
+        parser.join()
+
+    if not os.path.exists(os.curdir + '/data'):
+        os.mkdir(os.curdir + '/data')
+        print('Folder for data was created!')
+
+    with open('data/threats.json', 'w', encoding='utf-8') as file:
+        json.dump(threats, file, indent=4, ensure_ascii=False)
+
+
